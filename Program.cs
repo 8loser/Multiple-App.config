d88da3100ce968080ddc65b5244359cc0354e328
8loser <8loser@gmail.com>
2022-05-28 02:45:53 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string applicationSettingsValue = Properties.Settings.Default.keyName;
+            string appSettingsValue = ConfigurationManager.AppSettings["keyName"];
+            Console.WriteLine("applicationSettings keyName = "+ applicationSettingsValue);
+            Console.WriteLine("appSettings keyName = "+ appSettingsValue);
+            Console.ReadKey();
+        }
+    }
+}
